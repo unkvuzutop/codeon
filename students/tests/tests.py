6bbec4a523da46477e345f2217cb51df9f309e7f
inkvizitor <inkvizitor@corp>
2015-07-29 23:59:50 +0300

    add

--- students/tests/tests.py
+++ students/tests/tests.py
@@ -6,6 +6,9 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
+from codeon import settings
+from students.models import Group, Student
+
 
 class ModelsTestCase(TestCase):
     def setUp(self):
@@ -31,41 +34,49 @@ class ModelsTestCase(TestCase):
     def test_add_group_and_student(self):
         """
         1) Login
-        2) Create Group and check result
-        3) Create Student with created group_id and check result
+        2) Create Group
+        3) Check group present in DB
+        4) Check redirect after creation of the group
+        3) Create Student with created group_id
+        4) Check student present in DB
+        5) Check redirect after creation of the  student
         4) Get current Student list page and check count of objects
         on that page
 
         folder with test image /static/testimg.jpg
         """
         self.client.login(username='test', password='test')
+        group_data = {'title': 'Test group'}
         response = self.client.post(reverse('group_create'),
-                                    {'title': 'Test group'})
+                                    group_data)
+        get_latest_group = Group.objects.latest('id')
+
+        self.assertEqual(get_latest_group.title, group_data['title'])
         self.assertRedirects(response,
-                             '/studentlist/1',
+                             '/studentlist/' + str(get_latest_group.id),
                              status_code=302,
                              target_status_code=200,
                              fetch_redirect_response=True,
                              msg_prefix='Can\'t add group')
 
-        with open(os.getcwd()+'/static/testimg.jpg') as image:
-            form_data = {'full_name' : 'Test Full Name',
+        with open(os.getcwd() + '/static/testimg.jpg') as image:
+            form_data = {'full_name': 'Test Full Name',
                          'photo': image,
-                         'group': self.test_row_id,
+                         'group': str(get_latest_group.id),
                          'birth_date': datetime.strftime(datetime.now(),
                                                          '%Y-%m-%d %H:%M:%S'),
                          'student_ticket_number': 2355}
 
             response = self.client.post(reverse('student_create'), form_data)
 
+        get_latest_student = Student.objects.latest('id')
+        self.assertEqual(get_latest_student.full_name, form_data['full_name'])
         self.assertRedirects(response,
-                             '/studentlist/1',
+                             '/studentlist/' + str(get_latest_group.id),
                              status_code=302,
                              target_status_code=200,
                              fetch_redirect_response=True,
                              msg_prefix='Can\'t add user')
-
         response = self.client.get(reverse('student_list',
-                                           args=[self.test_row_id]))
-
+                                           args=[get_latest_group.id]))
         self.assertEqual(len(response.context['student_list']), 1)
